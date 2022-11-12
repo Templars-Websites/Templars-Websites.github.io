f5946c4e82d481f544c96a503f0bebb21bcd1ab2
Michael <michaelamiribrahim@gmail.com>
2022-11-12 16:29:44 +0100

    yes

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    extend: {
+      colors: {
+        darkBlue: '#1B1F3B',
+        medBlue: '#30358D',
+        ligBlue: '#454ADE',
+        darkPurple: '#7B4AE6',
+        ligPurple: '#B14AED',
+        ligPink: '#C874D9'
+      }
+    },
+  },
+  plugins: [],
+}
